ca71fd11c00d42580133999a3589600159125c99
Ross Albert Tejada <rtejada.biz@gmail.com>
2025-08-13 03:29:40 +0800

    feat: add file tree UI for sidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,20 +1,88 @@
+import { TreeView, type TreeDataItem } from '@/components/ui/tree-view';
+
+import {
+  ChevronRight,
+  File,
+  FilePlus2,
+  Folder,
+  FolderOpen,
+  FolderPlus,
+} from 'lucide-react';
+
+import { Button } from './ui/button';
+
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarRail,
 } from '@/components/ui/sidebar';
 
-export function AppSidebar() {
+const data: TreeDataItem[] = [
+  {
+    id: '1',
+    name: 'Item 1',
+    children: [
+      {
+        id: '2',
+        name: 'Item 1.1',
+        children: [
+          {
+            id: '3',
+            name: 'Item 1.1.1',
+          },
+          {
+            id: '4',
+            name: 'Item 1.1.2',
+          },
+        ],
+      },
+      {
+        id: '5',
+        name: 'Item 1.2 (disabled)',
+        disabled: true,
+      },
+    ],
+  },
+  {
+    id: '6',
+    name: 'Item 2 (draggable)',
+    draggable: true,
+  },
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar>
-      <SidebarHeader />
+    <Sidebar {...props}>
+      <SidebarHeader className="flex flex-row justify-center mt-2">
+        <Button variant="secondary" size="icon" className="size-8">
+          <FolderPlus />
+        </Button>
+        <Button variant="secondary" size="icon" className="size-8">
+          <FilePlus2 />
+        </Button>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
+        <SidebarGroupLabel>Notes</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <TreeView
+            data={data}
+            defaultLeafIcon={File}
+            defaultNodeIcon={Folder}
+          />
+          <SidebarMenu></SidebarMenu>
+        </SidebarGroupContent>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
   );
 }
