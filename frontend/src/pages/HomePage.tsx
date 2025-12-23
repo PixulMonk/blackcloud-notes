d6d884be470f2c749cbdcb382f0d30627e0f2d8f
Ross Albert Tejada <rtejada.biz@gmail.com>
2025-12-24 04:24:08 +0800

    style(UI): center editor and toolbar

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,8 +2,12 @@ import Editor from '@/components/Editor';
 
 function HomePage() {
   return (
-    <div>
-      <Editor />
+    <div className="flex flex-col w-full max-h-screen items-center p-2">
+      {/* TODO: h1 is a placeholder. To be replaced by document title */}
+      <div>
+        <h1 className="mx-4 text-4xl font-bold">Untitled</h1>
+        <Editor />
+      </div>
     </div>
   );
 }
