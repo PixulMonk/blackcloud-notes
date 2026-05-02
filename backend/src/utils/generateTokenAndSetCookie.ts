df716287cdcfa64a036ea9147d534e35011cd5e5
Ross Albert Tejada <rtejada.biz@gmail.com>
2026-05-02 09:50:03 +0800

    fix: resolve cross-site cookie rejection

--- backend/src/utils/generateTokenAndSetCookie.ts
+++ backend/src/utils/generateTokenAndSetCookie.ts
@@ -17,8 +17,8 @@ export const generateTokenAndSetCookie = (res: Response, userId: string) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
