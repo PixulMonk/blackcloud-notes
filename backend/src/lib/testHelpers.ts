7202b06e6b94ff5d2e79a46cad24310bd543c1d6
Ross Albert Tejada <rtejada.biz@gmail.com>
2026-06-07 22:51:03 +0800

    feat: add test for getLoginMetaData controller

---
+++ backend/src/lib/testHelpers.ts
@@ -0,0 +1,9 @@
+import { Response } from 'express';
+
+export const buildRes = (): Partial<Response> => {
+  const res: Partial<Response> = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  res.cookie = jest.fn().mockReturnValue(res);
+  return res;
+};
